fe2d3110d840540c91df810263901e2e22e432fb
P.Gerdenel <47711469+pgerdenel@users.noreply.github.com>
2021-09-22 11:05:39 +0200

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<?php
+
+echo'
+
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Interface</title>
+    <link rel="stylesheet" href="css/interface/font_google_api.css">
+    <link rel="stylesheet" media="all" href="css/interface/style1.css" />
+    <link rel="stylesheet" media="screen" href="css/interface/style2.css" />
+    <link rel="stylesheet" media="all" href="css/interface/style3.css" />
+    <link rel="stylesheet" media="screen" href="css/interface/style4.css" id="cm-theme" />
+</head>
+
+<body class="room-editor editor state-htmlOn-cssOn-jsOn sidebar-false preload prelayout layout-top logged-out ">
+
+';
